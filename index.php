61e04312cce4501eb64d1f5178ada02838c075b6
Pedro Passos <100157780+pedropassos22@users.noreply.github.com>
2023-02-01 11:57:20 -0300

    Update index.php

--- index.php
+++ index.php
@@ -13,23 +13,24 @@
 <body>
     <form action="#" method="POST" class="produtoresgeral">
         <fieldset>
-            <label>Nome:</label> <input type="text"><br> 
-            <label>CPF:</label> <input type="number"><br>
+            <label>Nome:</label> <input required type="text" name="nome"><br> 
+            <label>CPF:</label> <input required type="number" name="cpf"><br>
             
-            <fieldset name="sexo">
+           <fieldset name="sexo">
                 <p>Sexo:</p>
-                <label> <input type="radio" name="sexo" value="M"> Masculino </label>
-                <label> <input type="radio" name="sexo" value="F"> Feminino </label>
+                <label> <input required type="radio" name="sexo" value="M"> Masculino </label>
+                <label> <input required type="radio" name="sexo" value="F"> Feminino </label>
             </fieldset>
             
-            <label>SIAPEC:</label> <input type="number"><br>
-            <label>Endereço:</label> <input type="text"><br>
-            <label>Propriedade Rual:</label><input type="text"><br>
-            <label>Telefone 1:</label> <input type="number"> <br>
-            <label>Telefone 2:</label> <input type="number"> <br>
-            <label>Email:</label><input type="email"><br>
-            <label>Data de Atualização </label> <input type="date"><br><br><br>
-            <button type="submit">Enviar</button><br><br><br>
+            <label>SIAPEC:</label> <input type="number" name="siapec"><br>
+            <label>Endereço:</label> <input type="text" name="endereco"><br>
+            <label>Propriedade Rual:</label><input required type="text" name="pr"><br>
+            <label>Telefone 1:</label> <input required type="number" name="telum"> <br>
+            <label>Telefone 2:</label> <input type="number" name="teldois"> <br>
+            <label>Email:</label><input type="email" name="email"><br>
+            <label>Data de Atualização </label> <input required type="date" name="dataatualizacao"><br><br><br>
+            <button type="submit" name="enviar">Enviar</button><br><br><br>
+            
             <h1>Dados Enviados:</h1>
 
         </fieldset>
