9c4f4fd37e55d351c373999080e8f4b012a039e8
Pedro Passos <100157780+pedropassos22@users.noreply.github.com>
2023-02-01 13:58:33 -0300

    Update index.php

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 
 
 <body>
-    <form action="#" method="POST" class="produtoresgeral">
+    <form action="" method="POST" class="produtoresgeral">
         <fieldset>
             <label>Nome:</label> <input required type="text" name="nome"><br> 
             <label>CPF:</label> <input required type="number" name="cpf"><br>
@@ -32,6 +32,22 @@
             <button type="submit" name="enviar">Enviar</button><br><br><br>
             
             <h1>Dados Enviados:</h1>
+
+            <?php 
+                if(isset($_POST['enviar'])) {
+                    echo "<p><b>Nome: </b>" . $_POST['nome'] . "</p>";
+                    echo "<p><b> CPF: </b>" . $_POST['cpf'] . "</p>";
+                    echo "<p><b> Sexo: </b>" . $_POST['sexo'] . "</p>";
+                    echo "<p><b> SIAPEC: </b>" . $_POST['siapec'] . "</p>";
+                    echo "<p><b> Endereço: </b>" . $_POST['endereco'] . "</p>";
+                    echo "<p><b> Propriedade Rural: </b>" . $_POST['pr'] . "</p>";
+                    echo "<p><b> Telefone 1: </b>" . $_POST['telum'] . "</p>";
+                    echo "<p><b> Telefone 2: </b>" . $_POST['teldois'] . "</p>";
+                    echo "<p><b> Email: </b>" . $_POST['email'] . "</p>";
+                    echo "<p><b> Data de Atualização: </b>" . $_POST['dataatualizacao'] . "</p>";
+                }
+                
+            ?>
 
         </fieldset>
     </form>    
